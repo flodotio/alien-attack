37f4f4d0958a8f5720abbd6dfe0e98cd556777ba
flodotio <31792029+flodotio@users.noreply.github.com>
2026-03-08 19:52:06 +0100

    finished shooting rockets

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,11 @@ extends CharacterBody2D
 var speed = 300
 var rocket_scene = preload("res://scenes/rocket.tscn")
 
+@onready var rocket_container = $RocketContainer
+
+func _ready():
+	pass
+
 func _process(delta):
 	shoot_rocket()
 
@@ -27,5 +32,5 @@ func _physics_process(delta: float) -> void:
 func shoot_rocket():
 	if Input.is_action_just_pressed("shoot"):
 		var rocket_instance = rocket_scene.instantiate()
-		add_child(rocket_instance)
-		rocket_instance.global_position.x += 70
+		rocket_container.add_child(rocket_instance)
+		rocket_instance.global_position = Vector2(global_position.x + 70, global_position.y)
